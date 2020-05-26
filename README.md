2e9b4c01ddd6d2365f51fc3e8705074956ca3ca1
unknown <lemonwjj@163.com>
2020-05-26 15:24:36 +0800

    Signed-off-by: unknown <lemonwjj@163.com>

--- README.md
+++
@@ -1,1 +0,0 @@
-Here is the source code and some supplementary material for 'iSENSE2.0: Improving Completion-aware Crowdtesting Management with Semantic Analysis and Sanity Check'
--- src/MannWhitneyUTest.java
+++
@@ -1,8 +0,0 @@
-public class MannWhitneyUTest {
-	public void testDifference ( String fileName ) {
-		MannWhitneyUTest test = new MannWhitneyUTest();
-		
-		
-		//double pValue = test.mannWhitneyUTest(jValues, kValues);
-	}
-}
